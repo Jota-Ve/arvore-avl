ad55c1660071bfbb2a4522757e3f57a061e45c1c
João Vitor Barbosa <j.vs.b13@gmail.com>
2023-07-03 20:22:01 -0300

    Medindo tempo e criando estrutura de pastas do projeto

--- include/arvore-avl/AVL.hpp
+++ include/arvore-avl/AVL.hpp
@@ -1,55 +1,53 @@
 
-
-#pragma once
-
-#include <chrono> // NOLINT [build/c++11]
+#include <algorithm>
+#include <array>
+#include <chrono>
 #include <ctime>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
 #include <string>
 
-
-template <typename T> class No {
-public:
+template <typename T>
+class No {
+ public:
   T chave;
-  No *esquerda;
-  No *direita;
+  No* esquerda;
+
+  No* direita;
   int altura;
 
   // Construtor
-  No(const T &chave)
+  No(const T& chave)
       : chave(chave), esquerda(nullptr), direita(nullptr), altura(1) {}
 };
 
-template <typename T> class ArvoreAVL {
-private:
-  No<T> *raiz;
+template <typename T>
+class ArvoreAVL {
+ private:
+  No<T>* raiz;
 
   // Função para obter a altura de um nó
-  int altura_No(No<T> *no) {
-    if (no == nullptr)
-      return 0;
+  int altura_No(No<T>* no) {
+    if (no == nullptr) return 0;
     return no->altura;
   }
 
   // Função para obter o fator de balanceamento de um nó
-  int fator_B(No<T> *no) {
-    if (no == nullptr)
-      return 0;
+  int fator_B(No<T>* no) {
+    if (no == nullptr) return 0;
     return altura_No(no->esquerda) - altura_No(no->direita);
   }
 
   // Função para atualizar a altura de um nó
-  void att_Altura(No<T> *no) {
-    if (no == nullptr)
-      return;
+  void att_Altura(No<T>* no) {
+    if (no == nullptr) return;
     no->altura = std::max(altura_No(no->esquerda), altura_No(no->direita)) + 1;
   }
 
   // Função para fazer uma rotação simples à esquerda
-  No<T> *gira_esquerda(No<T> *no) {
-    No<T> *raizNova = no->direita;
+  No<T>* gira_esquerda(No<T>* no) {
+    No<T>* raizNova = no->direita;
     no->direita = raizNova->esquerda;
     raizNova->esquerda = no;
     att_Altura(no);
@@ -58,8 +56,8 @@ private:
   }
 
   // Função para fazer uma rotação simples à direita
-  No<T> *gira_direita(No<T> *no) {
-    No<T> *raizNova = no->esquerda;
+  No<T>* gira_direita(No<T>* no) {
+    No<T>* raizNova = no->esquerda;
     no->esquerda = raizNova->direita;
     raizNova->direita = no;
     att_Altura(no);
@@ -68,79 +66,93 @@ private:
   }
 
   // Função para fazer uma rotação dupla esquerda-direita
-  No<T> *gira_esquerdadireita(No<T> *no) {
+  No<T>* gira_esquerdadireita(No<T>* no) {
     no->esquerda = gira_esquerda(no->esquerda);
     return gira_direita(no);
   }
 
   // Função para fazer uma rotação dupla direita-esquerda
-  No<T> *gira_direitaesquerda(No<T> *no) {
+  No<T>* gira_direitaesquerda(No<T>* no) {
     no->direita = gira_direita(no->direita);
     return gira_esquerda(no);
   }
 
   // Função para balancear um nó
-  No<T> *balance(No<T> *no) {
-    if (no == nullptr)
-      return nullptr;
+  No<T>* balance(No<T>* no) {
+    if (no == nullptr) return nullptr;
 
     att_Altura(no);
 
     int balanceFactor = fator_B(no);
 
     if (balanceFactor > 1) {
-      if (fator_B(no->esquerda) >= 0)
+      if (fator_B(no->esquerda) >= 0) {
+        // std::cout << "rota direita" << std::endl;
         return gira_direita(no);
-      else
+      } else {
+        // std::cout << "dupla esquerda direita" << std::endl;
         return gira_esquerdadireita(no);
+      }
     } else if (balanceFactor < -1) {
-      if (fator_B(no->direita) <= 0)
+      if (fator_B(no->direita) <= 0) {
+        // std::cout << "rota esquerda" << std::endl;
         return gira_esquerda(no);
-      else
+      } else {
+        // std::cout << "dupla direita esquerda" << std::endl;
         return gira_direitaesquerda(no);
+      }
     }
-
+    // std::cout << " | fator balanceado = " << fator_B(no) << " | " <<
+    // std::endl;
     return no;
   }
 
   // Função auxiliar para inserir um nó na árvore
-  No<T> *inserir_No(No<T> *no, const T &chave) {
-    if (no == nullptr)
+  No<T>* inserir_No(No<T>* no, const T& chave) {
+    if (no == nullptr) {
+      // std::cout << "no vazio, criar um" << std::endl;
       return new No<T>(chave);
-
-    if (chave < no->chave)
+    }
+    if (chave < no->chave) {
       no->esquerda = inserir_No(no->esquerda, chave);
-    else if (chave > no->chave)
+      // std::cout << no->chave << "<-" << chave << std::endl;
+    }
+    if (chave > no->chave) {
       no->direita = inserir_No(no->direita, chave);
-    else
+      // std::cout << no->chave << "->" << chave << std::endl;
+    }
+    if (chave == no->chave) {
+      // std::cout << no->chave << "==" << chave << std::endl;
       return no;
+    }
 
     return balance(no);
   }
 
   // Função auxiliar para buscar um nó na árvore
-  No<T> *buscar_No(No<T> *no, const T &chave) {
-    if (no == nullptr || no->chave == chave)
+  No<T>* buscar_No(No<T>* no, const T& chave) {
+    if (no == nullptr || no->chave == chave) {
+      // std::cout << chave << " = " << no->chave << std::endl;
       return no;
-
-    if (chave < no->chave)
+    }
+    if (chave < no->chave) {
+      // std::cout << chave << " < " << no->chave << std::endl;
       return buscar_No(no->esquerda, chave);
-
+    }
+    // std::cout << chave << " > " << no->chave << std::endl;
     return buscar_No(no->direita, chave);
   }
 
   // Função auxiliar para encontrar o nó com menor valor
-  No<T> *encontrar_Minimo(No<T> *no) {
-    if (no == nullptr || no->esquerda == nullptr)
-      return no;
+  No<T>* encontrar_Minimo(No<T>* no) {
+    if (no == nullptr || no->esquerda == nullptr) return no;
 
     return encontrar_Minimo(no->esquerda);
   }
 
   // Função auxiliar para remover um nó da árvore
-  No<T> *remover_No(No<T> *no, const T &chave) {
-    if (no == nullptr)
-      return no;
+  No<T>* remover_No(No<T>* no, const T& chave) {
+    if (no == nullptr) return no;
 
     if (chave < no->chave)
       no->esquerda = remover_No(no->esquerda, chave);
@@ -148,55 +160,54 @@ private:
       no->direita = remover_No(no->direita, chave);
     else {
       if (no->esquerda == nullptr || no->direita == nullptr) {
-        No<T> *temp = no->esquerda ? no->esquerda : no->direita;
+        No<T>* temp = no->esquerda ? no->esquerda : no->direita;
 
         if (temp == nullptr) {
           temp = no;
           no = nullptr;
-        } else
+        } else {
           *no = *temp;
+        }
 
         delete temp;
+
       } else {
-        No<T> *temp = encontrar_Minimo(no->direita);
+        No<T>* temp = encontrar_Minimo(no->direita);
         no->chave = temp->chave;
         no->direita = remover_No(no->direita, temp->chave);
       }
     }
 
-    if (no == nullptr)
-      return no;
+    if (no == nullptr) return no;
 
     return balance(no);
   }
 
   // Função auxiliar para percorrer a árvore em ordem
-  void percorrer_Ordem(No<T> *no) {
-    if (no == nullptr)
-      return;
+  void percorrer_Ordem(No<T>* no) {
+    if (no == nullptr) return;
 
     percorrer_Ordem(no->esquerda);
     // std::cout << no->chave << " ";
     percorrer_Ordem(no->direita);
   }
 
-public:
+ public:
   // Construtor
   ArvoreAVL() : raiz(nullptr) {}
 
   // Função para inserir uma chave na árvore
-  void inserir(const T &chave) { raiz = inserir_No(raiz, chave); }
+  void inserir(const T& chave) { raiz = inserir_No(raiz, chave); }
 
   // Função para remover uma chave da árvore
-  void remover(const T &chave) { raiz = remover_No(raiz, chave); }
+  void remover(const T& chave) { raiz = remover_No(raiz, chave); }
 
   // Função para buscar uma chave na árvore
-  bool buscar(const T &chave) { return buscar_No(raiz, chave) != nullptr; }
+  bool buscar(const T& chave) { return buscar_No(raiz, chave) != nullptr; }
 
   // Função para percorrer a árvore em ordem
-  void AVL_em_Ordem(No<T> *no) {
-    if (no == nullptr)
-      return;
+  void AVL_em_Ordem(No<T>* no) {
+    if (no == nullptr) return;
     AVL_em_Ordem(no->esquerda);
     // std::cout << no->chave << " ";
     AVL_em_Ordem(no->direita);
@@ -205,81 +216,97 @@ public:
   // Função para percorrer a árvore em ordem
   void percorrer_Ordem() { AVL_em_Ordem(raiz); }
 
-  double medirTempoBusca(std::ofstream &arquivo) {
+  auto medirTempoBusca(std::ofstream& arquivo) {
     const int numExecucoes =
-        10000;  // Número de execuções para calcular a média de tempo
-    double tempoTotal = 0.0;
+        5000000;  // Número de execuções para calcular a média de tempo
+    // std::array<int, numExecucoes> arr{};
+    // for (int i = 0; i < arr.size(); i++) arr.at(i) = i;
+    // std::random_shuffle(arr.begin(), arr.end());
 
-    arquivo << std::fixed
-            << std::setprecision(
-                   10);  // Configura a precisão decimal para 10 casas
+    arquivo << "TamanhoArvore"
+            << ";"
+            << "NanoSeg"
+            << "\n";
 
-    for (int i = 0; i < numExecucoes; ++i) {
-      std::chrono::high_resolution_clock::time_point startTime =
-          std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < numExecucoes; i++) {
+      auto chave = std::to_string(i);
+      inserir(chave);
 
-      buscar(std::to_string(i));
+      auto inicio = std::chrono::steady_clock::now();
+      buscar(chave);
 
-      std::chrono::high_resolution_clock::time_point endTime =
-          std::chrono::high_resolution_clock::now();
-
-      std::chrono::duration<double, std::milli> elapsedTime =
-          endTime - startTime;
-        
-      tempoTotal += elapsedTime.count();
-      arquivo << i << ";" << elapsedTime.count()
-              << "\n";  // Grava o tempo com todas as casas decimais
+      auto fim = std::chrono::steady_clock::now();
+      auto duracao = fim - inicio;
+      auto NanoSeg =
+          std::chrono::duration_cast<std::chrono::nanoseconds>(duracao).count();
+      // tempoTotal += duracao;
+      arquivo << i << ";" << NanoSeg << "\n";
     }
 
-    double tempoMedio = tempoTotal / numExecucoes;
-    return tempoMedio;
+    return 0;
   }
 
-  double medirTempoInsercao(std::ofstream &arquivo) {
-    const int numExecucoes =
-        10000;  // Número de execuções para calcular a média de tempo
-    double tempoTotal = 0.0;
+  double medirTempoInsercao(std::ofstream& arquivo) {
+    // Número de execuções para calcular a média de tempo
+    const int numExecucoes = 5000000;
+    // std::array<int, numExecucoes> arr{};
 
-    arquivo << std::fixed
-            << std::setprecision(
-                   10);  // Configura a precisão decimal para 10 casas
+    // for (auto i = 0; i < arr.size(); i++) arr.at(i) = i;
+    // std::random_shuffle(arr.begin(), arr.end());
 
-    for (int i = 0; i < numExecucoes; ++i) {
-      std::chrono::high_resolution_clock::time_point startTime =
-          std::chrono::high_resolution_clock::now();
-      inserir(std::to_string(i));
-      std::chrono::high_resolution_clock::time_point endTime =
-          std::chrono::high_resolution_clock::now();
-      std::chrono::duration<double, std::milli> elapsedTime =
-          endTime - startTime;
-      tempoTotal += elapsedTime.count();
-      arquivo << i << ";" << elapsedTime.count() << "\n";
+    arquivo << "TamanhoArvore"
+            << ";"
+            << "NanoSeg"
+            << "\n";
+
+    for (int i = 0; i < numExecucoes; i++) {
+      auto chave = std::to_string(i);
+
+      auto inicio = std::chrono::steady_clock::now();  // NOLINT
+
+      inserir(chave);
+
+      // buscar(std::to_string(arr.at(i)));
+      auto fim = std::chrono::steady_clock::now();
+      auto duracao = fim - inicio;
+      auto NanoSeg =
+          std::chrono::duration_cast<std::chrono::nanoseconds>(duracao)
+              .count();
+      arquivo << i << ";" << NanoSeg << "\n";
     }
-    double tempoMedio = tempoTotal / numExecucoes;
-    return tempoMedio;
+
+    return 0;
   }
 
-  double medirTempoRemocao(std::ofstream &arquivo) {
-    const int numExecucoes =
-        10000;  // Número de execuções para calcular a média de tempo
-    double tempoTotal = 0.0;
+  double medirTempoRemocao(std::ofstream& arquivo) {
+    const int numExecucoes = 5000000;
+    // std::array<int, numExecucoes> arr{};
 
-    arquivo << std::fixed
-            << std::setprecision(
-                   10);  // Configura a precisão decimal para 10 casas
+    // for (auto i = 0; i < arr.size(); i++) arr.at(i) = i;
+    // std::random_shuffle(arr.begin(), arr.end());
 
-    for (int i = 0; i < numExecucoes; ++i) {
-      std::chrono::high_resolution_clock::time_point startTime =
-          std::chrono::high_resolution_clock::now();
-      remover(std::to_string(i));
-      std::chrono::high_resolution_clock::time_point endTime =
-          std::chrono::high_resolution_clock::now();
-      std::chrono::duration<double, std::milli> elapsedTime =
-          endTime - startTime;
-      tempoTotal += elapsedTime.count();
-      arquivo << i << ";" << elapsedTime.count() << "\n";
+    arquivo << "TamanhoArvore"
+            << ";"
+            << "NanoSeg"
+            << "\n";
+
+    for (int i = 0; i < numExecucoes; i++) {
+      auto chave = std::to_string(i);
+
+      auto inicio = std::chrono::steady_clock::now();
+      remover(chave);
+      // buscar(std::to_string(arr.at(i)));
+      auto fim = std::chrono::steady_clock::now();
+
+      auto duracao = fim - inicio;
+      auto NanoSeg =
+          std::chrono::duration_cast<std::chrono::nanoseconds>(duracao)
+              .count();
+
+      arquivo << i << ";" << NanoSeg << "\n";
     }
-    double tempoMedio = tempoTotal / numExecucoes;
-    return tempoMedio;
+  
+
+    return 0;
   }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,39 @@
-// SPDX-License-Identifier:  MIT
-// Copyright (C) 2023 - Prof. Igor Machado Coelho
-// ?(jota-ve): Devo atualizar o copyright acima para os integrantes do grupo
 
-#include <cassert>
+
+#include <algorithm>
+#include <array>
+#include <chrono>
+#include <ctime>
+#include <fstream>
+#include <iomanip>
 #include <iostream>
-#include <memory>
-//
-#include <fmt/core.h>  // NOLINT
+#include <string>
 
-// #include <../tests/test.hpp>
 #include "../include/arvore-avl/AVL.hpp"
 
-using fmt::print;
-
-
 int main() {
-    ArvoreAVL<std::string> arvore;
-    std::ofstream arquivo_i("tempos_Insercao.txt");
-    std::ofstream arquivo_b("tempos_Busca.txt");
-    std::ofstream arquivo_r("tempos_Remocao.txt");
-    // Inserir elementos na árvore
-    double tempoInsercao = arvore.medirTempoInsercao(arquivo_i);
-    arquivo_i.close();
-    // Inprimir a árvore
-    std::cout << "Árvore em ordem: ";
-    arvore.percorrer_Ordem();
-    std::cout << std::endl;
-    // Realizar uma busca
-    double tempoBusca = arvore.medirTempoBusca(arquivo_b);
-    arquivo_b.close();
-    // Remover um elemento da árvore
-    double tempoRemocao = arvore.medirTempoRemocao(arquivo_r);
-    arquivo_r.close();
+  ArvoreAVL<std::string> arvore;
+  std::ofstream arquivo_i("../benchmark/inserir.csv", std::ios::out);
+  std::ofstream arquivo_b("../benchmark/buscar.csv", std::ios::out);
+  std::ofstream arquivo_r("../benchmark/remover.csv", std::ios::out);
 
-    std::cout << "Tempo medio de insercao: " << tempoInsercao << " milisegundos" << std::endl;
-    std::cout << "Tempo medio de remocao: " << tempoRemocao << " milisegundos" << std::endl;
-    std::cout << "Tempo medio de busca: " << tempoBusca << " milisegundos" << std::endl;
+  // Número de execuções para calcular a média de tempo
+  const int numExecucoes = 500000;
 
-    return 0;
+  // Inserir elementos na árvore
+  std::cout << "Medindo inserção\n";
+  double tempoInsercao = arvore.medirTempoInsercao(arquivo_i);
+  arquivo_i.close();
+
+  // Remover um elemento da árvore
+  std::cout << "Medindo Remoção\n";
+  double tempoRemocao = arvore.medirTempoRemocao(arquivo_r);
+  arquivo_r.close();
+
+  // Realizar uma busca
+  std::cout << "Medindo Busca\n";
+  double tempoBusca = arvore.medirTempoBusca(arquivo_b);
+  arquivo_b.close();
+
+  return 0;
 }
-
-// Ctrl+Shift+P
-// CMake: build
-// CMake: configure
-// CMake: debug
-// [aplicacao]
